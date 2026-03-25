2d476ca7fb5ab0e09ab564e3b945c22a6c4e2d28
Eddie <eddie@vasser.no>
2026-03-25 18:55:38 +0100

    remove plausible script 🙈

--- snippets/header.php
+++ snippets/header.php
@@ -36,14 +36,6 @@
     '/media/plugins/eddiedale/index-theme/css/prism.css'
   ]) ?>
 
-  <?php if (!$kirby->user()) : ?>
-  <script async src="https://plausible.io/js/pa-CDt4qX-m5MLLNVMHjP4HB.js"></script>
-  <script>
-    window.plausible=window.plausible||function(){(plausible.q=plausible.q||[]).push(arguments)},plausible.init=plausible.init||function(i){plausible.o=i||{}};
-    plausible.init()
-  </script>
-  <?php endif; ?>
-
 
 </head>
 
