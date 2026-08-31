710dcd8a213935dff930008677717d1f0bcccef8
Eddie <eddie@vasser.no>
2026-08-31 14:17:03 +0200

    Remove shortform-post handling (date-slug hook, uuid create block, hidden titles)

--- snippets/blog-posts.php
+++ snippets/blog-posts.php
@@ -48,9 +48,7 @@ if($style === 'index') {
             </time>
         </div>
 
-          <?php if (!preg_match('/^\d{4}-\d{2}-\d{2}(-\d+)?$/', $article->title()->value())): ?>
           <h2><a href="<?= $article->url() ?>"><?= $article->title()->esc() ?></a></h2>
-          <?php endif ?>
 
           <?= $article->text_content()->kt() ?>
 
